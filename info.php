769730421fa94b7dfd9736af3161dfdd3595c7ed
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-11 13:54:20 +0530

    Update info.php

--- info.php
+++ info.php
@@ -9,5 +9,10 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+$sql = "select id, name from details";
+$result = $conn->query($sql);
+while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ". "<br>";
+  }
 mysqli_close($conn);
 ?>
