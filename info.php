20c7a6ff03299f6a2142d2989854975964d3514c
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-11 13:58:39 +0530

    Update info.php

--- info.php
+++ info.php
@@ -11,14 +11,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+echo "Details"."<br><br>";
 $sql = "SELECT id, name FROM details";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
   }
 } else {
   echo "0 results";
