78b286b1285be7a21a5b97abaf3f23d07351fac8
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-11 13:34:30 +0530

    Update info.php

--- info.php
+++ info.php
@@ -4,5 +4,11 @@
  $dbpass = "root";
  $db = "sample";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- echo "done1";
+ echo "done";
+$stat = $conn->query('select name form details');
+foreach($statement as $row)
+{
+echo $row['name']
+}
+
 ?>
