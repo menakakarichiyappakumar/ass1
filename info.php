a93f374db69975da3fd92112e8248729e30f9f78
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-11 13:41:17 +0530

    Update info.php

--- info.php
+++ info.php
@@ -3,7 +3,6 @@
  $dbuser = "root";
  $dbpass = "root";
  $db = "sample";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) 
- echo "helloo";
- 
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ echo "done";
 ?>
