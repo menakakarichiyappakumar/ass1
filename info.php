eeb23bd0f64aebda9ea82f3bceca4e407a899544
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-11 13:29:51 +0530

    Update info.php

--- info.php
+++ info.php
@@ -1,8 +1,16 @@
 <?php
-$conn = mysqli_connect("localhost", "", "root");
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-?>
-
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "root";
+ $db = "sample";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
